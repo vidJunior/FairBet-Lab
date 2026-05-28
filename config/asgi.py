2c9e26269e7093590b7d2019f17d5c9c83dab2f4
nsilupu-30 <nsilupu03@gmail.com>
2026-05-28 03:21:25 -0500

    fix: habilitar el servicio de archivos estaticos en ASGI/Daphne para restaurar estilos del admin

--- config/asgi.py
+++ config/asgi.py
@@ -3,10 +3,16 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
+from django.conf import settings
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 # Inicializar Django ASGI antes de importar ruteadores que usan modelos
 django_asgi_app = get_asgi_application()
+
+if settings.DEBUG:
+    django_asgi_app = ASGIStaticFilesHandler(django_asgi_app)
 
 import betting.routing
 

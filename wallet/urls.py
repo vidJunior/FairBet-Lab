bd9e8ed9e47125c4b55fb57b351e4a8ad5f2209f
ManuelCarrasco28 <josemanuelcarrascomillan@gmail.com>
2026-05-30 03:55:34 -0500

    feat: agregar endpoints y transacciones de billetera

--- wallet/urls.py
+++ wallet/urls.py
@@ -3,6 +3,7 @@ from wallet.views import (
     BilleteraHTMLView,
     RecargaHTMLView,
     RetiroHTMLView,
+    ReclamarCodigoBonoHTMLView,
 )
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path("", BilleteraHTMLView.as_view(), name="wallet_billetera"),
     path("recargar/", RecargaHTMLView.as_view(), name="wallet_recargar"),
     path("retirar/", RetiroHTMLView.as_view(), name="wallet_retirar"),
+    path("reclamar-codigo/", ReclamarCodigoBonoHTMLView.as_view(), name="wallet_reclamar_codigo"),
 ]
